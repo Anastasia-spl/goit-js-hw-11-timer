1aaafd065a91317d2830f572aebd9a8934ab39e7
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-02-05 14:00:35 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -4,36 +4,44 @@ class CountdownTimer {
     constructor({ selector, targetDate }) {
         this.selector = selector;
         this.targetDate = targetDate;
+        this.timerOutputRef = document.querySelector(`${this.selector}`);
     }
     
     startTimer() {
-        const timerOutputRef = document.querySelector(`${this.selector}`);
-        updateClockFace(0, timerOutputRef);
+        const nullifiedTimer = this.formatTimeRecord(0);
+        this.updateTimerFace(this.timerOutputRef, nullifiedTimer);
+
         const targetTime = this.targetDate.getTime();
 
         setInterval(() => {
             const currentTime = Date.now();
             const deltaTime = targetTime - currentTime;
-            updateClockFace(deltaTime, timerOutputRef);
+            const timer = this.formatTimeRecord(deltaTime);
+            this.updateTimerFace(this.timerOutputRef, timer);
         }, 1000);
+    }
+
+    formatTimeRecord(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        return {days, hours, mins, secs};
+    }
+    
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+
+    updateTimerFace(timerFaceRef, { days, hours, mins, secs }) {
+        timerFaceRef.querySelector('span[data-value="days"]').textContent = days;
+        timerFaceRef.querySelector('span[data-value="hours"]').textContent = hours;
+        timerFaceRef.querySelector('span[data-value="mins"]').textContent = mins;
+        timerFaceRef.querySelector('span[data-value="secs"]').textContent = secs;
     }
 }
 
-function updateClockFace(time, timerFaceRef) {
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    timerFaceRef.querySelector('span[data-value="days"]').textContent = days;
-    timerFaceRef.querySelector('span[data-value="hours"]').textContent = hours;
-    timerFaceRef.querySelector('span[data-value="mins"]').textContent = mins;
-    timerFaceRef.querySelector('span[data-value="secs"]').textContent = secs;
-}
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-}
 
 const summerTimer = new CountdownTimer({
   selector: '#timer-1',
